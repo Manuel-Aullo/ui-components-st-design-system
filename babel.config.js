4a012c989874b98795cecdbfcf8dcb3905a26026
manuel aullo <manuel.aullo@cybercom.com>
2021-06-21 14:11:32 +0300

    feat: modified babel config to include only the desired files

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = (api) => {
+	api.cache(true)
+	return {
+		ignore: [
+			"babel.config.js",
+			"dist",
+			"node_modules",
+			"src/components/*.stories.js",
+			"src/templates",
+			"src/**/*.mdx",
+			"src/stories",
+			"src/index.js"
+		]
+	}
+}
